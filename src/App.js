353b13711cd2cbf943fd131085726a7d4306eef4
Ross <rosskeene196@gmail.com>
2022-12-19 16:23:43 -0600

    add player working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import RankingTable from './components/Rankingtable';
 
 const App = () => {
+  const formRef = useRef(null);
   const [players, setPlayers] = useState([
     {name: 'Alice', bagTag: 1},
     {name: 'Bob', bagTag: 2},
     {name: 'Charlie', bagTag: 3},
   ]);
-
   const updateRankings = (players, player, bagTag) => {
+   
     // Make a copy of the players array so we don't modify the original
     const updatedPlayers = [...players];
+  
+    // Check if there is already a player in the array with the same name and bag tag
+    const duplicatePlayer = updatedPlayers.find(p => p.name === player.name && p.bagTag === bagTag);
+  
+    // If there is a duplicate player, return the original players array
+    if (duplicatePlayer) {
+      return players;
+    }
   
     // Find the index of the player in the array
     const playerIndex = updatedPlayers.findIndex(p => p.name === player.name);
   
-    // Update the player's bag tag
-    updatedPlayers[playerIndex] = {
-      ...player,
-      bagTag,
-    };
+    // If the player is not in the array, add them to the end
+    if (playerIndex === -1) {
+      updatedPlayers.push(player);
+    } else {
+      // Otherwise, update the player's bag tag
+      updatedPlayers[playerIndex] = {
+        ...player,
+        bagTag,
+      };
+    }
   
     // Sort the players by bag tag
     updatedPlayers.sort((a, b) => a.bagTag - b.bagTag);
@@ -37,7 +51,7 @@ const App = () => {
   };
   
 
-  const onAddPlayer = (event) => {
+  const onAddPlayer = (event, formRef) => {
     event.preventDefault();
     const name = event.target.name.value;
     const bagTag = parseInt(event.target.bagTag.value, 10); // parse bagTag as integer
@@ -46,16 +60,20 @@ const App = () => {
       const newPlayer = { name, bagTag };
       const updatedPlayers = updateRankings(players, newPlayer, 0);
       setPlayers(updatedPlayers);
+  
+      // Reset the form after the player is added
+      formRef.current.reset();
     } catch (error) {
       console.error(error);
     }
   };
   
+  
 
   return (
     <div>
       <RankingTable players={players} onRoundSubmit={onRoundSubmit} />
-      <form onSubmit={event => onAddPlayer(event)}>
+      <form ref={formRef} onSubmit={event => onAddPlayer(event, formRef)}>
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" />
         <label htmlFor="bagTag">Bag Tag:</label>
