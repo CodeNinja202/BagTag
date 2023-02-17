586c86aadcb19a7b17e34be8e6fb486a397a49a6
Ross <rosskeene196@gmail.com>
2023-02-17 11:07:00 -0600

    added extra token

--- api/users.js
+++ api/users.js
@@ -1,63 +1,58 @@
 const express = require("express");
 const router = express.Router();
-
-
-//requires all user Database functions
 const {
-  getUserByUsername,
   createUser,
+  getUserByUsername,
   getAllUsers,
   getUserById,
-  getUser,
 } = require("../db/users");
-///////////////////////////////////////////////////
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const { JWT_SECRET }  = process.env;//rquires the secret
-const { requireUser } = require('./utils')//throws an error if a user is not logged in
+const refreshTokens = {};
+const JWT_SECRET = process.env.JWT_SECRET;
 
-
-const bcrypt = require("bcrypt");//encrypts the password
-const jwt = require("jsonwebtoken");// reuqires the token
-
-
-//Get all users from the database
 router.get("/", async (req, res, next) => {
   try {
     const allUsers = await getAllUsers();
-
     res.send(allUsers);
   } catch (error) {
     next(error);
   }
 });
-//////////////////////////////////////////////
 
-//Login route
-router.post('/login', async (req, res, next) => {
-  
+router.post("/login", async (req, res, next) => {
   try {
     const { username, password } = req.body;
-    console.log("TESTING USERNAME LINE 30", username);
+
     if (!username || !password) {
       res.send({
         name: "MissingCredentialsError",
-        message: "Please supply both a username and password"
+        message: "Please supply both a username and password",
       });
     }
+
     const user = await getUserByUsername(username);
-    console.log("TESTING USER______", user);
     const hashedPassword = user.password;
     const isValid = await bcrypt.compare(password, hashedPassword);
-    console.log(process.env.JWT_SECRET)
+
     if (user && isValid) {
-      const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET);
+      // Generate access and refresh tokens
+      const token = jwt.sign({ id: user.id, username }, JWT_SECRET, {
+        expiresIn: "3s",
+      });
+      const refreshToken = jwt.sign({ id: user.id, username }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
 
-      res.send({ message: "you're logged in!", token });
+      // Store the refresh token on the server and associate it with the user's ID
+      refreshTokens[refreshToken] = user.id;
 
+      res.send({ message: "you're logged in!", token, refreshToken });
     } else {
       next({
-        name: 'IncorrectCredentialsError',
-        message: 'Username or password is incorrect'
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
       });
     }
   } catch (error) {
@@ -65,12 +60,8 @@ router.post('/login', async (req, res, next) => {
     next(error);
   }
 });
-////////////////////////////////////////////////////////////////
 
-
-
-//Registers a user to the database
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   const { username, password, email } = req.body;
 
   try {
@@ -78,52 +69,57 @@ router.post('/register', async (req, res, next) => {
     const user = await createUser({
       email,
       username,
-      password
+      password,
     });
 
     if (_user) {
       res.send({
-        error: 'error',
-        name: 'UserExistsError',
-        message: `A user by that ${username} already exists`
+        error: "error",
+        name: "UserExistsError",
+        message: `A user by that ${username} already exists`,
       });
     }
 
-    const token = jwt.sign({
-      id: user.id,
-      username
-    }, JWT_SECRET, {
-      expiresIn: '1w'
+    // Generate access and refresh tokens
+    const token = jwt.sign({ id: user.id, username }, JWT_SECRET, {
+      expiresIn: "3s",
     });
+    const refreshToken = jwt.sign({ id: user.id, username }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    // Store the refresh token on the server and associate it with the user's ID
+    refreshTokens[refreshToken] = user.id;
 
     res.send({
-      message: "you're signed up!", token, user
+      message: "you're signed up!",
+      token,
+      refreshToken,
     });
-
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
-////////////////////////////////////////////////////////////////
 
-//Reuires a user from the server
-router.get('/me', requireUser, async (req, res, next) => {
-    const user = req.user;
-    console.log(user)
-    res.send(user);
-  
-    next();
-  
+
+
+
+
+
+
+router.get("/me", async (req, res, next) => {
+  const user = req.user;
+  console.log(user);
+  res.send(user);
+
+  next();
 });
 
-
-
-//Gets a user by ID
-router.get('/:id', requireUser, async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const user = await getUserById(id);
-    console.log("GETTING USERS HERE", user)
+    console.log("GETTING USERS HERE", user);
     res.send(user);
   } catch (error) {
     console.log(error);
@@ -131,5 +127,37 @@ router.get('/:id', requireUser, async (req, res, next) => {
   }
 });
 
+
+
+router.post("/refresh", async (req, res, next) => {
+  try {
+    // Get the refresh token from the request body
+    const refreshToken = req.body.refreshToken;
+
+    // Check if the refresh token is valid (i.e. it exists in the "refreshTokens" object)
+    if (!refreshToken || !refreshTokens[refreshToken]) {
+      throw {
+        name: "InvalidTokenError",
+        message: "Invalid or expired refresh token",
+      };
+    }
+
+    // Get the user ID associated with the refresh token
+    const userId = refreshTokens[refreshToken];
+
+    // Get the user from the database
+    const user = await getUserById(userId);
+
+    // Generate a new JWT for the user
+    const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, {
+      expiresIn: "3s",
+    });
+
+    // Send the new JWT to the client
+    res.send({ token });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
