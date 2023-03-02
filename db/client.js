3042fe02ba71f58ddaf354de932000080d2fc953
Ross <rosskeene196@gmail.com>
2023-03-02 07:37:21 -0600

    saved

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/bagTag';
+const connectionString = `${process.env.DATABASE_URL}` || 'https://localhost:5432/bagTag';
 
 const client = new Client({
   connectionString,
